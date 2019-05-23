ceda4b6157eb4c402f0bf88c79785406cda49bcc
AlinCMT <climente.alin@gmail.com>
2019-05-23 16:20:19 +0300

    if no listings then show add new listing btn moved scripts.js for anunturi

--- base_app/static/base_app/scripts.js
+++ base_app/static/base_app/scripts.js
@@ -103,27 +103,3 @@ for (var i = 0; i < favs.length; i++) {
 }
 
 
-
-// Split facilitati into individual chips
-
-try {
-
-let facilitati = document.getElementById("facilitati").innerText.split(",");
-let other_details = document.getElementById("other-details");
-
-for(let i=0; i < facilitati.length; i++){
-    
-    let div = document.createElement('div');
-    div.className += "chip blue-grey darken-2 white-text";
-    div.innerText = facilitati[i];
-    other_details.appendChild(div);
-
-    // <div class="chip blue-grey darken-2 white-text">{{ anunt.get_tip_imobil_display }}</div>
-}
-
-document.getElementById("facilitati").remove();
-    
-} catch (error) {
-
-}
-
