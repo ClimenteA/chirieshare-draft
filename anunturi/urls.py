68cca9573ff0d42153baa7e3d828d1523bec0a15
AlinCMT <climente.alin@gmail.com>
2019-05-29 16:25:01 +0300

    must find a way to get id_anunt..

--- anunturi/urls.py
+++ anunturi/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('adauga/', views.adauga, name='adauga'),
     path('sheriasi/<int:id_anunt>', views.sheriasi, name='sheriasi'),
+    path('chirias/', views.chirias, name='chirias'),
 
 ]
