2a84d9fa8dfaac2825ea216b7f4c921717d6b1f3
AlinCMT <climente.alin@gmail.com>
2019-05-30 16:07:07 +0300

    extracted the js and css from anunturi templates into a separate static files

---
+++ anunturi/static/anunturi/scripts.js
@@ -0,0 +1,153 @@
+"use strict";
+
+//Carousel functions
+document.addEventListener('DOMContentLoaded', () => {
+    let carouselElems = document.querySelector('.carousel.carousel-slider');
+    let carouselInstance = M.Carousel.init(carouselElems, {
+            fullWidth: true,
+            indicators: false
+        });
+}, false);
+
+
+document.getElementById("next").addEventListener("click", () => {
+    let elems = document.querySelector('.carousel.carousel-slider');
+    let moveRight = M.Carousel.getInstance(elems);
+    moveRight.next();
+}, false);
+
+document.getElementById("prev").addEventListener("click", () => {
+    let elems = document.querySelector('.carousel.carousel-slider');
+    let moveLeft = M.Carousel.getInstance(elems);
+    moveLeft.prev();
+}, false);
+
+
+// Split facilitati into individual chips
+document.addEventListener('DOMContentLoaded', () => {
+
+    let facilitati = document.getElementById("facilitati").innerText.split(",");
+    let other_details = document.getElementById("other-details");
+
+    for(let i=0; i < facilitati.length; i++){
+        
+        let div = document.createElement('div');
+        div.className += "chip blue-grey darken-2 white-text";
+        div.innerText = facilitati[i];
+        other_details.appendChild(div);
+        // <div class="chip blue-grey darken-2 white-text">{{ anunt.get_tip_imobil_display }}</div>
+    }
+    document.getElementById("facilitati").remove();
+
+}, false);
+
+
+//COPY TO CLIPBOARD
+
+var clipboard = new ClipboardJS('.btn');
+
+clipboard.on('success', () => {
+    M.toast({html: 'Copiat!'});
+});
+
+clipboard.on('error', (e) => {
+    M.toast({html: 'Copiere nereusita!'});
+});
+
+
+// Create colegi de share chips
+
+
+function createAddMeBtn(response){
+    
+    let root = document.getElementById("colegideshare");
+
+    if (!(response.current_user_added)) {
+    console.log("this user is not in sheriasi");
+    // <button class="btn-floating btn waves-effect waves-light green tooltipped" data-position="top" data-tooltip="click pe button daca ai vrea sa imparti chiria pentru acest imobil"><i class="material-icons">add</i></button>
+    const Add = {
+        view: () => {
+            return m('button', {onclick: () => {
+                console.log('adauga ma la share!');
+            }, "class":"btn-floating btn waves-effect waves-light green tooltipped",
+                "data-position": "top",
+                "data-tooltip":"vreau sa impart chiria pentru acest imobil cu alti colegi"
+                }, [
+                    m("i", {"class": "material-icons"}, "add")
+                ])
+        }
+    };
+
+    m.mount(root, Add);
+    M.Tooltip.init(document.querySelectorAll('.tooltipped'));
+    let container_sheriasi = document.createElement('div');
+    container_sheriasi.setAttribute("id", "sheriasi");
+    root.appendChild(container_sheriasi);
+    
+    let addbtn = root.getElementsByTagName("button")[0];
+    addbtn.style.position = 'absolute';
+    addbtn.style.bottom = '2rem';
+    addbtn.style.right = '2rem';
+
+}
+
+}
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    let root = document.getElementById("colegideshare");
+    let id_anunt = document.location.pathname.split("/")["2"];
+
+    m.request({
+        method: "GET",
+        url: `/anunturi/sheriasi/${id_anunt}`,
+    }).then((response) => {
+        
+        root.classList = [];
+
+        if (response.status === 200) {
+            colegideshare = response.colegideshare;
+            // console.log("colegideshare", colegideshare);
+            
+            createAddMeBtn(response);
+            
+            // <a href="#contact-info"><div class="chip blue-grey darken-2 white-text"><img src="http://placeskull.com/100/100/4caf50">Cosmin Tana</div></a> 
+
+            let urlroot = document.baseURI.replace(document.location.pathname, "/cont/utilizator/");
+            let id_anunt = document.location.pathname.split("/")["2"];
+
+            const Sherias = {
+                    view: () => {
+                        return m("div", 
+                            colegideshare.map((u) => { return m("a", {href: String(urlroot + u.id + "/" + id_anunt)}, [
+                                    m("div", {class: "chip blue-grey darken-2 white-text"}, [
+                                        m("img", {src: (u.imagine != "") ? u.imagine : "http://placeskull.com/100/100/4caf50"}),
+                                        m("span", (u.first_name != "") ? u.first_name : u.email.split('@')[0])
+                                    ])
+                                ])
+
+                            })
+                        )
+                    }
+                };
+
+            m.mount(document.getElementById("sheriasi"), Sherias);
+
+        }
+        else if (response.status === 204) {
+            createAddMeBtn(response);
+            const Nouser = {
+                view: () => {
+                    return m("h6", "Fi primul la share! Apasa buttonul din dreapta jos!")
+                }
+            };
+
+            m.mount(document.getElementById("sheriasi"), Nouser);
+
+        }
+    });
+
+}, false);
+
+
